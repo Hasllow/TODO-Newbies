b5639a8126c986f0045cdaa22f5f2275d3444c04
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-04-10 16:58:30 -0300

    JS - Adiciona script

---
+++ tasks.js
@@ -0,0 +1,16 @@
+let tasksData = [
+  {
+    id: 1,
+    name: "Ver se eu tô na esquina",
+    toDo: true,
+  },
+  {
+    id: 2,
+    name: "Dar banho nos gatos",
+    toDo: false,
+  },
+];
+
+const addTaskInput = document.getElementById("task_input");
+const addTaskButton = document.getElementsByTagName("button")[0];
+const taskList = document.getElementById("tasks_list");
