8f61a93561f57ef38835b436197837e09f937506
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-04-10 18:01:42 -0300

    JS - Cria task element

--- tasks.js
+++ tasks.js
@@ -19,13 +19,58 @@ const addTaskInput = document.getElementById("task_input");
 const addTaskButton = document.getElementsByTagName("button")[0];
 const taskList = document.getElementById("tasks_list");
 
+// Create new task element
+function createNewTaskEl(taskName, taskId) {
+  let task = document.createElement("li");
+  task.classList.add("task");
+  task.classList.add("todo");
+  task.setAttribute("id", taskId);
+
+  let leftContent = document.createElement("div");
+  leftContent.classList.add("left_content");
+
+  let todoIcon = document.createElement("i");
+  todoIcon.classList.add("ph-duotone");
+  todoIcon.classList.add("ph-circle-dashed");
+  todoIcon.classList.add("check_btn");
+  todoIcon.addEventListener("click", completeTask());
+
+  let doneIcon = document.createElement("i");
+  doneIcon.classList.add("ph-duotone");
+  doneIcon.classList.add("ph-check-circle");
+  doneIcon.classList.add("check_btn");
+  doneIcon.classList.add("hidden");
+  doneIcon.addEventListener("click", incompleteTask());
+
+  let name = document.createElement("p");
+  name.innerHTML = taskName;
+
+  let deleteIcon = document.createElement("i");
+  deleteIcon.classList.add("ph-duotone");
+  deleteIcon.classList.add("ph-trash");
+  deleteIcon.classList.add("delete_btn");
+  deleteIcon.addEventListener("click", deleteTask());
+
+  leftContent.appendChild(todoIcon);
+  leftContent.appendChild(doneIcon);
+  leftContent.appendChild(name);
+
+  task.appendChild(leftContent);
+  task.appendChild(deleteIcon);
+
+  return task;
+}
+
 // Add new task
 function addTask(event) {
   event.preventDefault();
 
   const newTaskName = addTaskInput.value;
   const newTask = { id: uid(), name: newTaskName, toDo: true };
+
   tasksData.push(newTask);
+  const taskElement = createNewTaskEl(newTask.name, newTask.id);
+  taskList.appendChild(taskElement);
 }
 // Complete task
 function completeTask(event) {
