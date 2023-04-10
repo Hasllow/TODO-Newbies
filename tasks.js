13807c0f69dae617f26f5fb61afce3dd6f782f5a
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-04-10 17:17:33 -0300

    JS - Adiciona funções básicas

--- tasks.js
+++ tasks.js
@@ -14,3 +14,21 @@ let tasksData = [
 const addTaskInput = document.getElementById("task_input");
 const addTaskButton = document.getElementsByTagName("button")[0];
 const taskList = document.getElementById("tasks_list");
+
+// Add new task
+function addTask(event) {
+  event.preventDefault();
+  console.log("Task adicionada");
+}
+// Complete task
+function completeTask(event) {
+  console.log("Task marcada como completa");
+}
+// Incomplete task
+function incompleteTask(event) {
+  console.log("Task marcada como incompleta");
+}
+// Delete task
+function deleteTask(event) {
+  console.log("Task deletada");
+}
